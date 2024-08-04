c4ff316bb8a7054f9194f711fa42627a806a617b
harsh7298 <harshmehta4000@gmail.com>
2024-08-04 10:21:28 +0530

    commited changes till 1:56:00 of the vid.

---
+++ components/Input/Input.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Style from './Input.module.css';
+/*errors because code video mein dikha hi nhi*/
+const Input=({inputT})=>{
+    return( 
+    <div className={Style.input}>
+        <p>{title}</p>
+        {inputType ==="text" ? string 
+            <div className={Style.input__box}>
+                <input type='text' className={Style.input__box__form} placeholder={placeholder} onChange={handleClick}/>
+            </div>
+        )}
+    </div>):()
+
+
+};
+export default Input;
